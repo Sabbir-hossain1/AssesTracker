6bd3c4769fa43b1d36fa651c133b09c89358d5e8
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2023-06-03 16:25:34 +0600

    Companies model added , view folder added for separate views and urls.py for all compnay related urls

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Model for companies
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    slogan = models.CharField(max_length=50)
+    description = models.TextField()
+    logo = models.ImageField(upload_to='company_logo/')
+    establish_date = models.DateField()
+    website = models.URLField()
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
